d9f99b609eb91689226f0573e09fee4766490149
skyy6 <freddee97@gmail.com>
2020-10-08 14:18:38 +0200

    la till movement + camerafix

---
+++ PADME_Project/Assets/Scripts/MouseView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseView : MonoBehaviour
+{
+    float xRotation = 0f;
+    public Transform playerBody;
+    public float sensitivity = 100f;
+    
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ PADME_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController playerController;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public float movementSpeed = 14f;
+    public LayerMask groundMask;
+    public float jumpHeight = 2.5f;
+
+
+    Vector3 velocity;
+    bool isGrounded;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        playerController.Move(move * movementSpeed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        playerController.Move(velocity * Time.deltaTime);
+    }
+}
