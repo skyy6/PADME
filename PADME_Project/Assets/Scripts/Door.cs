df84c991f29afc0b036d511b6b33881447949969
alve917 <72448020+alve917@users.noreply.github.com>
2020-10-12 08:59:15 +0200

    OpenDoor
    Lade till en dörr som går att öppna.

---
+++ PADME_Project/Assets/Scripts/Door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public float speed;
+    private bool triggered = false;
+
+    public Vector3 openPosition;
+    private Vector3 closedPosition;
+
+    void openObject()
+    {
+        Debug.Log("Opening");
+        triggered = true;
+    }
+
+    void Start()
+    {
+        closedPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if(triggered)
+        {
+            if (transform.position.z > openPosition.z)
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+        }                   
+    }
+}
---
+++ PADME_Project/Assets/Scripts/OnEnterOpen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnterOpen : MonoBehaviour
+{
+    public GameObject obj;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Player")
+        {
+            obj.SendMessage("openObject");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            obj.SendMessage("openObject");
+        }
+    }
+}
