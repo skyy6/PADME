2f50c1487ab00ae8955ae8e809b1e6594dd03b8d
skyy6 <freddee97@gmail.com>
2020-10-23 12:36:23 +0200

    bättre pickup

--- PADME_Project/Assets/Scripts/PickUp.cs
+++ PADME_Project/Assets/Scripts/PickUp.cs
@@ -10,6 +10,7 @@ public class PickUp : MonoBehaviour
     Rigidbody rigidBody;
     Vector3 startScale;
     bool isCarrying = false;
+   
 
     private void Start()
     {
@@ -17,17 +18,20 @@ public class PickUp : MonoBehaviour
         playerCamera = GameObject.Find("Main Camera").transform;
         rigidBody = GetComponent<Rigidbody>();
         startScale = transform.localScale;
+       
+
     }
 
     private void OnMouseOver()
     {
         float distance = Vector3.Distance(player.position, transform.position);
         //Debug.Log("Looking at " + transform.name + "Distance: " + distance);
-        if(Input.GetKey(KeyCode.E) && distance < 3)
+        if(Input.GetKey(KeyCode.E) && distance < 3f)
         {
             transform.parent = playerCamera;
             rigidBody.isKinematic = true;
             isCarrying = true;
+            
 
         }
 
