6fc8ce7f4040eab8743fe7c7dd2d946dd2c0af6a
skyy6 <freddee97@gmail.com>
2020-11-01 16:26:55 +0100

    ändrat particles

--- PADME_Project/Assets/Scripts/CameraBehavior.cs
+++ PADME_Project/Assets/Scripts/CameraBehavior.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class CameraBehavior : MonoBehaviour
 {
     GameObject sparkSystem;
+    Transform sparkPos;
+    bool destroyed = false;
 
 
     void Start()
     {
         sparkSystem = GameObject.Find("Sparks");
+        sparkPos = GameObject.Find("Sparks").transform;
+        //Physics.IgnoreCollision(sparkPos.GetComponent<Collider>(), transform.GetComponent<Collider>());
     }
     private void OnCollisionEnter(Collision collision)
     {
+        //sparkPos.parent = transform;
         sparkSystem.GetComponent<ParticleSystem>().Play();
+        destroyed = true;
+        //sparkPos = transform;
 
     }
 
 
     void Update()
     {
-        
+        //Debug.Log(destroyed);
+       /* if (destroyed)
+        {
+            //sparkPos.parent = transform;
+        }*/
     }
 }
