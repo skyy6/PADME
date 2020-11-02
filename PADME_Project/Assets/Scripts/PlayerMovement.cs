059e6bd3257cf9688de3d8da72bea36bb88e9a05
skyy6 <freddee97@gmail.com>
2020-11-02 17:34:47 +0100

    Crouch + meck med particles

--- PADME_Project/Assets/Scripts/PlayerMovement.cs
+++ PADME_Project/Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,28 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController playerController;
+    
     public float gravity = -9.81f;
     public Transform groundCheck;
+    public Transform headCheck;
     float groundDistance = 0.2f;
     public float movementSpeed;
     public float walkingSpeed = 5f;
-    public float runningSpeed = 7f;
+    public float runningSpeed = 8f;
+    float originalHeight;
     public LayerMask groundMask;
     public float jumpHeight = 2.5f;
     public float fieldOfView = 60f;
     float timer = 2f;
     public int sprintTime = 800;
-    //public int maxSprintTime = 1500;
-    public Text myText;
+    float crouchingSpeed = 3f;
     Vector3 velocity;
-    bool isGrounded;
     bool moving;
+    bool isGrounded;
     bool isRunning = false;
-
+    bool isObstructed;
+    bool isCrouching;
+    bool isWalking;
     public StaminaBar staminaBar;
     int maxStamina = 800;
 
@@ -43,53 +47,112 @@ public class PlayerMovement : MonoBehaviour
         playerController.Move(velocity * Time.deltaTime);
     }
 
+    void Crouching()
+    {
+        playerController.height = 1f;
+        isWalking = false;
+        isRunning = false;
+        isCrouching = true;
+        
+    }
+    void Standing()
+    {
+        playerController.height = originalHeight;
+        isCrouching = false;
+        isWalking = true;
+      
+    }
+
+    void startRunning()
+    {
+      
+        float MaxFOV = fieldOfView + 10.0f;  //fov för mer realistisk sprint
+        sprintTime--;
+        isRunning = true;
+        timer = 2f;
+
+        if (Camera.main.fieldOfView < MaxFOV)
+        {
+            Camera.main.fieldOfView += 100 * Time.deltaTime; //increase fov
+        }
+    }
+
+
+    void startWalking()
+    {
+        isRunning = false;
+        isWalking = true;
+        if (Camera.main.fieldOfView > fieldOfView)
+        {
+            Camera.main.fieldOfView -= 80 * Time.deltaTime; //decrease fov
+        }
+
+
+    }
+    void Crouch()
+    {
+        isObstructed = Physics.CheckSphere(headCheck.position, groundDistance, groundMask);
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            Crouching();
+        }
+        else if (!Input.GetKeyDown(KeyCode.LeftControl) && !isObstructed)
+        {
+            Standing();
+        }
+    }
     void Run()
     {
-        staminaBar.SetStamina(sprintTime);
- 
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W) && sprintTime > 0) 
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W) && sprintTime > 0)
         {
-            float MaxFOV = fieldOfView + 10.0f;  //fov för mer realistisk sprint
-            movementSpeed = runningSpeed;
-            sprintTime--;
-            isRunning = true;
-            timer = 2f;
-
-            if (Camera.main.fieldOfView < MaxFOV)
-            {
-                Camera.main.fieldOfView += 100 * Time.deltaTime; //increase fov
-            }
+            startRunning();
         }
         else
         {
-            movementSpeed = walkingSpeed;
-            if (Camera.main.fieldOfView > fieldOfView)
+            startWalking();
+            if (sprintTime < 800)
             {
-                Camera.main.fieldOfView -= 80 * Time.deltaTime; //decrease fov
+                timer -= Time.deltaTime;
+                if (!moving | !Input.GetKey(KeyCode.LeftShift) && timer < 0)
+                {
+                    sprintTime++;
+
+                }
             }
         }
-        if (sprintTime < 800)
-        {
-            timer -= Time.deltaTime;
-            if (!moving | !Input.GetKey(KeyCode.LeftShift) && timer < 0)
-            {
-                sprintTime++;
-                
-            }    
-        }
-
     }
+
     void Start()
     {
         Camera.main.fieldOfView = fieldOfView;
-        //currentStamina = maxStamina;
         staminaBar.SetMaxStamina(maxStamina);
+        originalHeight = playerController.height;
+
 
     }
     void Update()
     {
+
+        staminaBar.SetStamina(sprintTime);
         Jumping();
         Run();
+        Crouch();
+
+        //Debug.Log(movementSpeed);
+
+        if (isWalking)
+        {
+            movementSpeed = walkingSpeed;
+        }
+        if (isCrouching)
+        {
+            movementSpeed = crouchingSpeed;
+        }
+        if (isRunning)
+        {
+            movementSpeed = runningSpeed;
+        }
+
 
        //myText = GameObject.Find("Text").GetComponent<Text>();
         //myText.text = "Units: " + sprintTime;
@@ -108,7 +171,5 @@ public class PlayerMovement : MonoBehaviour
 
         playerController.Move(move * movementSpeed * Time.deltaTime);
   
-        //Debug.Log(z);
-        //Debug.Log(moving);
     }
 }
