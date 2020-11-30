27551d4791da41d4a07aa299c3683fb3501b5a35
alve917 <72448020+alve917@users.noreply.github.com>
2020-11-30 14:48:56 +0100

    Light flicker

---
+++ PADME_Project/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+
+    public Light light;
+    public float minIntensity = 0f;
+    public float maxIntensity = 1f;
+    public float minFlickerSpeed = 0.1f;
+    public float maxFlickerSpeed = 0.5f;
+
+    private bool flicker = true;
+
+    private void Start()
+    {
+        if(light == null)
+        {
+            light = GetComponent<Light>();
+        }
+        StartCoroutine(Flicker());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Flicker()
+    {
+        yield return new WaitForSecondsRealtime(Random.Range(minFlickerSpeed, maxFlickerSpeed));
+        light.intensity = Random.Range(minIntensity, maxIntensity);
+        if(flicker)
+        {
+            StartCoroutine(Flicker());
+        }
+    }
+}
