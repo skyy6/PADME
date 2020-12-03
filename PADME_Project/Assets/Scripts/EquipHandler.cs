2df99e6cb2f4189001ec7cde708541127e741dc4
skyy6 <freddee97@gmail.com>
2020-12-03 16:17:14 +0100

    Animation Crowbar, ändrat equips

---
+++ PADME_Project/Assets/Scripts/EquipHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipHandler : MonoBehaviour
+{
+    public GameObject prefab;
+    Rigidbody rb;
+    Animator anim;
+    
+
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+    }
+
+
+    void Update()
+    {
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+        rb.isKinematic = true;
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.Play("crowbarAttack");
+        }
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.G))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+          
+        }
+    }
+}
---
+++ PADME_Project/Assets/Scripts/EquipItem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipItem : MonoBehaviour
+{
+    Transform cam;
+    Transform player;
+    Transform contain;
+    GameObject pos;
+    public GameObject crow;
+    Rigidbody rb;
+    bool equipped;
+
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").transform;
+        player = GameObject.Find("Controller").transform;
+        contain = GameObject.Find("Container").transform;
+        
+        rb = GetComponent<Rigidbody>();
+        equipped = false;
+        
+
+    }
+    private void OnMouseOver()
+    {
+
+        float distance = Vector3.Distance(player.position, transform.position);
+        if (Input.GetKeyDown(KeyCode.E) && distance < 3f)
+        {
+            equipped = true;
+            Instantiate(crow, contain.position, contain.rotation, contain);
+            Destroy(gameObject);
+        }
+    }
+    void Update()
+    {
+
+    }
+}
